f058cb9f572b3f54a720f44985317df23c2319c8
francisco <167242231+frangamer24@users.noreply.github.com>
2024-10-27 20:01:07 -0300

    actividad 5 (rotación)

---
+++ actividad 5 (rotacion)/actividad 5 (rotacion)/actividad5.cpp
@@ -0,0 +1,45 @@
+#include <SFML/window.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+
+	RenderWindow ventana(VideoMode(800, 600, 32), "rotacion de un sprite");
+
+	Texture textura;
+
+	if (!textura.loadFromFile("C:/SFML/imagenes/cuad_yellow.png"))
+	{
+		return 0;
+	}
+
+	Sprite sprite;
+	sprite.setTexture(textura);
+
+	sprite.setOrigin(sprite.getTexture()->getSize().x / 2, sprite.getTexture()->getSize().y / 2);
+
+	sprite.setPosition(400, 300);
+
+	while(ventana.isOpen())
+	{
+		Event evento;
+
+		while(ventana.pollEvent(evento))
+		{
+			if (evento.type == Event::Closed)
+				ventana.close();
+		}
+
+		ventana.clear();
+
+		sprite.rotate(5);
+
+		ventana.draw(sprite);
+
+		ventana.display();
+
+	}
+	return 0;
+}
