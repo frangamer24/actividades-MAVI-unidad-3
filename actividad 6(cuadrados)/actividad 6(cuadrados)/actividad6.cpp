8a5c85ab0c872bf444a9f9d8b8d7f45baa8594ea
francisco <167242231+frangamer24@users.noreply.github.com>
2024-10-27 20:02:48 -0300

    actividad 6(cuadrados)

---
+++ actividad 6(cuadrados)/actividad 6(cuadrados)/actividad6.cpp
@@ -0,0 +1,60 @@
+#include <SFML/Window.hpp>
+#include<SFML/Graphics.hpp>}
+
+using namespace std;
+using namespace sf;
+
+int main() 
+{
+	RenderWindow ventana(VideoMode(800, 600, 32), "cuadrados de colores");
+
+	Texture textura[4];
+	Sprite sprites[4];
+
+	std::string archivos[] = { "C:/SFML/imagenes/cuad_red.png", "C:/SFML/imagenes/cuad_yellow.png", "C:/SFML/imagenes/cuad_blue.png", "C:/SFML/imagenes/chessb.png" };
+	for (int i = 0; i < 4; i++)
+	{
+		if (!textura[i].loadFromFile(archivos[i])) 
+		{
+			return 0;
+		}
+
+		sprites[i].setTexture(textura[i]);
+
+		float escalaX = 200.0f / textura[i].getSize().x;
+		float escalaY = 200.0f / textura[i].getSize().y;
+		sprites[i].setScale(escalaX, escalaY);
+		
+		sprites[i].setPosition(100 + i * 210, 100);
+
+	}
+	float ancho = 200;
+	float alto = 200;
+
+	sprites[0].setPosition(100, 100);
+	sprites[1].setPosition(300, 100);
+	sprites[2].setPosition(100, 300);
+	sprites[3].setPosition(300, 300);
+	
+	while (ventana.isOpen()) 
+	{
+		Event evento;
+
+		while (ventana.pollEvent(evento)) 
+		{
+			if (evento.type == Event::Closed)
+				ventana.close();
+		}
+		ventana.clear();
+
+		for (int i = 0; i < 4;i++)
+		{
+			ventana.draw(sprites[i]);
+		}
+
+		ventana.display();
+	}
+
+	return 0;
+
+}
