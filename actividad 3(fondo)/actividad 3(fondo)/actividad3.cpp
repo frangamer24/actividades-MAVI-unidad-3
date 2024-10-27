b72aa6508907300900e05b0b40a64d3f76337e81
francisco <167242231+frangamer24@users.noreply.github.com>
2024-10-27 19:58:12 -0300

    actividad 3 (fondo)

---
+++ actividad 3(fondo)/actividad 3(fondo)/actividad3.cpp
@@ -0,0 +1,46 @@
+#include <SFML/window.hpp>
+#include <SFML/graphics.hpp>
+#include<iostream>
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	Texture texturafondo;
+	string fondoruta = "C:/SFML/imagenes/fondo.jpg";
+
+	if (!texturafondo.loadFromFile(fondoruta))
+	{
+		cerr << "error al cargar el fondo" << fondoruta << endl;
+		return 0;
+	}
+
+	Sprite spritefondo;
+	spritefondo.setTexture(texturafondo);
+
+	float anchoventana = 800;
+	float altoventana = 600;
+	float escalaX = anchoventana / texturafondo.getSize().x;
+	float escalaY = altoventana / texturafondo.getSize().y;
+	spritefondo.setScale(escalaX, escalaY);
+
+	RenderWindow ventana(VideoMode(800, 600, 32), "fondo del juego");
+
+	while(ventana.isOpen())
+	{
+		Event evento;
+
+		while (ventana.pollEvent(evento)) 
+		{
+			if (evento.type == Event::Closed)
+				ventana.close();
+		}
+
+		ventana.clear();
+
+		ventana.draw(spritefondo);
+
+		ventana.display();
+	}
+	return 0;
+}
