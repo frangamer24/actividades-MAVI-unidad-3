65684a65ecd8a9ba9a1a9b2b3f3562e52ee239a1
francisco <167242231+frangamer24@users.noreply.github.com>
2024-10-27 19:54:10 -0300

    actividad 1(Sniper)

---
+++ actividad 1(Sniper)/actividad 1(Sniper)/actividad1.cpp
@@ -0,0 +1,41 @@
+#include <SFML/window.hpp>
+#include <SFML/graphics.hpp>
+using namespace sf;
+
+Texture circulo;
+Texture textura;
+Sprite sprite;
+Sprite circulorojo;
+Sprite circulorojo1;
+Sprite circulorojo2;
+Sprite circulorojo3;
+
+int main()
+{
+	circulo.loadFromFile("C:/SFML/imagenes/rcircle.png");
+
+	circulorojo.setTexture(circulo);
+	circulorojo1.setTexture(circulo);
+	circulorojo2.setTexture(circulo);
+	circulorojo3.setTexture(circulo);
+
+	sf::RenderWindow App(sf::VideoMode(800, 600, 32), "cuatro sirculos rojos");
+
+	circulorojo1.setPosition(App.getSize().x - circulorojo1.getTexture()->getSize().x, 0);
+	circulorojo2.setPosition(0, App.getSize().y - circulorojo2.getTexture()->getSize().y);
+	circulorojo3.setPosition(App.getSize().x - circulorojo3.getTexture()->getSize().x, App.getSize().y - circulorojo2.getTexture()->getSize().y);
+
+	while (App.isOpen()) 
+	{
+
+		App.clear();
+
+		App.draw(circulorojo);
+		App.draw(circulorojo1);
+		App.draw(circulorojo2);
+		App.draw(circulorojo3);
+
+		App.display();
+	}
+	return 0;
+}
